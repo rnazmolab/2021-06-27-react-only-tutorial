e932d92ec59e9e20aa6033a0d759c04698c21632
rnazmo <rnazmo@gmail.com>
2021-06-27 05:46:11 +0900

    feat: Use Hooks to manage the state

--- src/Tweet.jsx
+++ src/Tweet.jsx
@@ -1,4 +1,10 @@
 function Tweet(props) {
+  const [liked, setLike] = React.useState(false);
+  const toggleLike = React.useCallback(
+    () => setLike((prev) => !prev),
+    [setLike]
+  );
+
   return (
     <div className="tweet">
       <div className="icon-container">{props.icon}</div>
@@ -8,6 +14,9 @@ function Tweet(props) {
           <span className="account-name">@{props.accountName}</span>
         </div>
         <div className="content">{props.content}</div>
+        <div className="status-action">
+          <span onClick={toggleLike}>{liked ? "❤️" : "♡"}</span>
+        </div>
       </div>
     </div>
   );
